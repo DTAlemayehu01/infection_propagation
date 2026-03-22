f825b25a7b3da377e9ec10e161f68faf7dcbfbd7
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2026-03-22 13:51:01 -0600

    scatter plot spread

--- src/infection_propagation/Figures.py
+++ src/infection_propagation/Figures.py
@@ -1,4 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from itertools import product
+from scipy import stats
+from collections import defaultdict
 
 # Assuming Dict Data
 def time_distribution(path_times):
@@ -25,3 +29,36 @@ def time_per_path(path_times):
         _,_,bars = axs[i].hist(path_times[path], bins="rice")
         axs[i].set_title(f"Infection time distribution, condtioned on path {path}")
         axs[i].bar_label(bars)
+        
+# Only really relevant on iid edges?
+def nodes_vs_time(path_times):
+    path_lens = []
+    path_len_times = []
+    for key in path_times.keys():
+        pts = list(product([len(key)-1], path_times[key]))
+        path_lens = path_lens + [pt[0] for pt in pts]
+        path_len_times = path_len_times + [pt[1] for pt in pts]
+        
+    path_lens = np.array(path_lens)
+    path_len_times = np.array(path_len_times)
+
+    slope, intercept, r, p, err = stats.linregress(path_lens, path_len_times)    
+    lin_fit = slope*path_lens + intercept
+    
+    time_len_dict = defaultdict(list)
+    for pt in zip(path_lens, path_len_times):
+        time_len_dict[pt[0]] = time_len_dict[pt[0]] + [pt[1]]
+    
+    ordered_keys = sorted(time_len_dict.keys())
+    for x in ordered_keys:
+        times = time_len_dict[x]
+        times = np.array(times)
+        print(f"Path Length = {x} Units: Average={np.mean(times)}, Variance={np.var(times)}, Min={np.min(times)}, Max={np.max(times)}")
+
+    plt.scatter(path_lens, path_len_times)
+    plt.plot(path_lens, lin_fit, label=f"LSRL, r={r: .2f}, p={p: .2f}, std-err={err: .2f}", color='orange', linestyle='--')
+    plt.title("Infection Time vs Path Length")
+    plt.xlabel("Path Length")
+    plt.ylabel("Infection Time")
+    plt.legend()
+    return r, p, err
